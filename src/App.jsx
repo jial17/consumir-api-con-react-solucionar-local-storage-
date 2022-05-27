0c31d03bf950c1aa8d7a7dbb4a1c044cf6a162df
jial17 <jial17@gmail.com>
2022-05-27 06:53:53 -0500

    api rick & morty

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import Formulario from "./components/Formulario";
+import PintarDatos from "./components/PintarDatos";
+
+const App = () => {
+  const [nombrePersonaje, setNombrePersonaje] = useState("");
+
+  /* useEffect(() => {
+    if (localStorage.getItem("nombreApi")) {
+      setNombrePersonaje(JSON.parse(localStorage.getItem("nombreApi")));
+    }
+    localStorage.setItem("nombreApi", JSON.stringify(nombrePersonaje));
+  }, [nombrePersonaje]); */
+
+  return (
+    <div className="container">
+      <h1>APP RICK & MORTY</h1>
+      <Formulario setNombrePersonaje={setNombrePersonaje} />
+      <PintarDatos nombrePersonaje={nombrePersonaje} />
+    </div>
+  );
+};
+
+export default App;
